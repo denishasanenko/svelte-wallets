d7e2415a3b82d90500fff727035f6d7c2c8ae846
Denis Hasanenko <denis.hasanenko@gmail.com>
2020-01-04 01:28:53 +0200

    Add code coverage

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ module.exports = {
     bail: false,
     verbose: true,
     transformIgnorePatterns: ["node_modules"],
-    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    testResultsProcessor: "jest-junit"
 };
